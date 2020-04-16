38bbbbed9cbcfb9b7b0c6474813a4e53865f3aa3
dmitrii92 <fogot92@gmail.com>
2020-04-16 17:21:56 +0300

    Updates

---
+++ precache-manifest.029e02b05ced6af208e3510e00b296fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3da067975f0c5cc79207c2eaaa79e4",
+    "url": "/react-statemanagement-contextapi-hooks-ts-starter/index.html"
+  },
+  {
+    "revision": "2e14f2583f8f27183e9f",
+    "url": "/react-statemanagement-contextapi-hooks-ts-starter/static/css/main.1cffb808.chunk.css"
+  },
+  {
+    "revision": "29c54ecaa01b66e96522",
+    "url": "/react-statemanagement-contextapi-hooks-ts-starter/static/js/2.6693228a.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/react-statemanagement-contextapi-hooks-ts-starter/static/js/2.6693228a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e14f2583f8f27183e9f",
+    "url": "/react-statemanagement-contextapi-hooks-ts-starter/static/js/main.4859b50e.chunk.js"
+  },
+  {
+    "revision": "70edceabd59c1d6b637e",
+    "url": "/react-statemanagement-contextapi-hooks-ts-starter/static/js/runtime-main.fee49e2b.js"
+  }
+]);
