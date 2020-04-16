82f5ccb1d3e29952fe3887e158f1a573a4d275d0
dmitrii92 <fogot92@gmail.com>
2020-04-16 21:43:21 +0300

    Add remove function.

--- src/components/view/Item.tsx
+++ src/components/view/Item.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Employee } from "../../interfaces"
+import Context from "../../context";
 
 export const Item: React.FC<Employee> = (employee) => {
+  // const {removeEmployee } = useContext(Context);
+
   return (
-    <tr>
-      <td>{employee.firstName}</td>
-      <td>{employee.lastName}</td>
-      <td>{employee.age}</td>
-    </tr>
+    <Context.Consumer>
+      {context => (
+        <tr>
+          <td>{employee.firstName}</td>
+          <td>{employee.lastName}</td>
+          <td>{employee.age}</td>
+          <td><button onClick={() => context?.removeEmployee(employee.id)}>&times;</button></td>
+        </tr>
+      )}
+    </Context.Consumer>
   );
 }
